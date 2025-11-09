402f40a2c965509fc126cba24d1443db1a55fccc
thanhtungdev05 <tungdt5101@ut.edu.vn>
2025-11-09 22:49:15 +0700

    up mode

--- site1/home/models.py
+++ site1/home/models.py
@@ -145,3 +145,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tour = models.ForeignKey('Tour', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'tour')  # tránh trùng yêu thích cùng tour
+
+    def __str__(self):
+        return f"{self.user.username} - {self.tour.title}"
